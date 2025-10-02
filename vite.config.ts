9e46af0ff27dfcca571b93efca98cd4d1c29c489
Radek Burget <burgetr@fit.vutbr.cz>
2025-10-02 12:40:32 +0200

    Export CSS correctly

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
     }
   },
   build: {
+    cssCodeSplit: false,
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.ts'),
