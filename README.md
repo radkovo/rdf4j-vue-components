4087b4bc9a88df57295d51eda8fd8ac0c4aa2186
Radek Burget <burgetr@fit.vutbr.cz>
2025-10-03 18:32:00 +0200

    Documentation update

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 (c) Radek Burget 2025
 
+A library of vue.js components for interacting with the RDF4J API. It includes an API client,
+query editor, query result display components and some more auxiliary components. These components
+are used in several projects such as [rdf4j-vue](https://github.com/radkovo/rdf4j-vue)
+or [FitLayout PageView](https://github.com/FitLayout/PageView).
--- vite.config.ts
+++ vite.config.ts
@@ -23,19 +23,15 @@ export default defineConfig({
   build: {
     cssCodeSplit: false,
     lib: {
-      // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'Rdf4jVueComponents',
-      // the proper extensions will be added
       fileName: 'rdf4j-vue-components',
     },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
+      // externalized deps that shouldn't be bundled into the library
       external: ['vue', 'primevue', 'primeicons', '@primevue/themes', 'n3', 'prismjs','sparqljs', 'vue-prism-editor'],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
+        // Global variables to use in the UMD build for externalized deps
         globals: {
           vue: 'Vue',
           primevue: 'PrimeVue',
